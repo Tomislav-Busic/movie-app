608118f9f3df5be4a8f1c6c4bf1e0026229f84f5
Tomislav Busic <tomislav.busic89@gmail.com>
2023-05-30 17:50:58 +0200

    React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import "./styles/app.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
+      <Router>
+        <Routes>
+          <Route />
+          <Route />
+          <Route />
+        </Routes>
+      </Router>
     </div>
   );
 }
