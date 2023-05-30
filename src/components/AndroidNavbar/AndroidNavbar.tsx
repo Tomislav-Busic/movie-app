2698056937af4ab038fe509b2f2f1cfafa533e34
Tomislav Busic <tomislav.busic89@gmail.com>
2023-05-30 21:04:07 +0200

    AndroidNavbar style

--- src/components/AndroidNavbar/AndroidNavbar.tsx
+++ src/components/AndroidNavbar/AndroidNavbar.tsx
@@ -3,7 +3,7 @@ import styled from "./androidNavbar.module.scss";
 export const AndroidNavbar = () => {
     return (
         <div className={styled.and_navbar}>
-            <div></div>
+            <div className={styled.buttons}></div>
         </div>
     )
 }
