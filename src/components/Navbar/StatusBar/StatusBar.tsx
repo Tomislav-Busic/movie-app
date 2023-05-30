38ef19b51372524e0a89d8c45769df333f95f867
Tomislav Busic <tomislav.busic89@gmail.com>
2023-05-30 20:26:25 +0200

    navbar statusBar

--- src/components/Navbar/StatusBar/StatusBar.tsx
+++ src/components/Navbar/StatusBar/StatusBar.tsx
@@ -1,9 +1,20 @@
 import styled from "./statusBar.module.scss";
 
+import battery from "../../../assets/images/battery.png";
+import cellular from "../../../assets/images/cellular.png";
+import time from "../../../assets/images/time.png";
+import wifi from "../../../assets/images/wifi.png";
+
+
 export const StatusBar = () => {
     return (
         <div className={styled.status}>
-            <div className={styled.bar}></div>
+            <div className={styled.bar}>
+                <img src={wifi} alt="wifi" />
+                <img src={cellular} alt="cellular" />
+                <img src={battery} alt="battery" />
+                <img src={time} alt="time" />
+            </div>
         </div>
     );
 }
