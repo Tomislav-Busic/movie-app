c6821347403b9d56acc0cc78087c44339f786b3c
Tomislav Busic <tomislav.busic89@gmail.com>
2023-05-30 20:48:00 +0200

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { Navbar } from "./components/Navbar/Navbar";
 import "./styles/app.scss";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/app.scss";
+import { HomePage } from "./pages/HomePage/HomePage";
+import { FavoritesPage } from "./pages/FavoritesPage/FavoritesPage";
+import { MoviePage } from "./pages/MoviePage/MoviePage";
 
 function App() {
   return (
@@ -9,9 +12,9 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route />
-          <Route />
-          <Route />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="/movie/:id" element={<MoviePage />} />
         </Routes>
       </Router>
     </div>
