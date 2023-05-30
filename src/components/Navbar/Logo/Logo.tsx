2aaf2e1be4210650636ab507a88e4a1a8f3428d8
Tomislav Busic <tomislav.busic89@gmail.com>
2023-05-30 19:57:31 +0200

    logo

--- src/components/Navbar/Logo/Logo.tsx
+++ src/components/Navbar/Logo/Logo.tsx
@@ -1,7 +1,9 @@
 import logo from "../../../assets/images/TMDB-Logo.png";
+import styled from "./logo.module.scss";
+
 export const Logo = () => {
     return (
-        <div>
+        <div className={styled.logo}>
             <img src={logo} alt="logo" />
         </div>
     );
