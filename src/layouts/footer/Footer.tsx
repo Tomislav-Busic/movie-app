8fddf3d451f459ee4171e6c9d2adc65109ba0f9b
Tomislav Busic <tomislav.busic89@gmail.com>
2023-06-01 18:17:39 +0200

    frame

--- src/layouts/footer/Footer.tsx
+++ src/layouts/footer/Footer.tsx
@@ -2,6 +2,11 @@ import styled from "./footer.module.scss";
 
 export const Footer = () => {
     return (
-        <div className={styled.footer}></div>
+        <div className={styled.footer}>
+            <div className={styled.container}>
+                <div className={styled.frame}></div>
+                <div className={styled.frame}></div>
+            </div>
+        </div>
     )
 }
