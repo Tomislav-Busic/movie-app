54ad4bddd007bd2c44eef4c76d37e3cf6361db6c
Tomislav Busic <tomislav.busic89@gmail.com>
2023-06-01 18:46:00 +0200

    react icons

--- src/layouts/footer/Footer.tsx
+++ src/layouts/footer/Footer.tsx
@@ -1,11 +1,22 @@
+import homeB from "../../assets/images/home-blue.png";
+import homeW from "../../assets/images/home-white.png";
+import favoritesB from "../../assets/images/favorites-blue.png";
+import favoritesW from "../../assets/images/favorites-white.png";
+
 import styled from "./footer.module.scss";
 
 export const Footer = () => {
     return (
         <div className={styled.footer}>
             <div className={styled.container}>
-                <div className={styled.frame}></div>
-                <div className={styled.frame}></div>
+                <div className={styled.frame}>
+                    <img src={homeB} alt="home" />
+                    <p>Home</p>
+                </div>
+                <div className={styled.frame}>
+                    <img src={favoritesW} alt="favorites" />
+                    <p>Favorites</p>
+                </div>
             </div>
         </div>
     )
