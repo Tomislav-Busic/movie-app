c8125a1e4c1bad177d7bf091cbc8b820d3296ec2
Tomislav Busic <tomislav.busic89@gmail.com>
2023-06-01 18:46:59 +0200

    AndroidNavbar.tsx

--- src/layouts/androidNavbar/AndroidNavbar.tsx
+++ src/layouts/androidNavbar/AndroidNavbar.tsx
@@ -9,15 +9,15 @@ export const AndroidNavbar = () => {
         <div className={styled.and_navbar}>
             <div className={styled.buttons}>
                 <div className={styled.img_container}>
-                    <img src={line} alt={line} />
-                    <img src={line} alt={line} />
-                    <img src={line} alt={line} />
+                    <img src={line} alt="line" />
+                    <img src={line} alt="line" />
+                    <img src={line} alt="line" />
                 </div>
                 <div className={styled.img_container}>
-                    <img src={vector} alt={vector} />
+                    <img src={vector} alt="vector" />
                 </div>
                 <div className={styled.img_container}>
-                    <img src={back} alt={back} />
+                    <img src={back} alt="back" />
                 </div>
             </div>
         </div>
