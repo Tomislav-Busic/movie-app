217daa2a7a08070623bc096078eec2ca4300fe77
Tomislav Busic <tomislav.busic89@gmail.com>
2023-06-02 21:01:00 +0200

    androidNavbar.module.scss

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -2,6 +2,32 @@ export const Home = () => {
     return (
         <>
             <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
         </>
     );
 }
