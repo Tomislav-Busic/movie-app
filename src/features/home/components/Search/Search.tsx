864e2371bdba2d64596e72697410180abf41da1c
Tomislav Busic <tomislav.busic89@gmail.com>
2023-06-02 22:11:47 +0200

    search

--- src/features/home/components/Search/Search.tsx
+++ src/features/home/components/Search/Search.tsx
@@ -1,9 +1,13 @@
+import { AiOutlineSearch, AiOutlineClose } from "react-icons/ai";
+
 import styled from "./search.module.scss";
 
 export const Search = () => {
     return (
         <div className={styled.search}>
-
+            <AiOutlineSearch />
+            <input placeholder="Search" />
+            <AiOutlineClose />
         </div>
     )
 }
