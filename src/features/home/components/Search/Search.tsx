7fdcb8445fe32106294e23964c86ebdd15fea9fe
Tomislav Busic <tomislav.busic89@gmail.com>
2023-06-02 22:55:14 +0200

    search

--- src/features/home/components/Search/Search.tsx
+++ src/features/home/components/Search/Search.tsx
@@ -4,10 +4,13 @@ import styled from "./search.module.scss";
 
 export const Search = () => {
     return (
-        <div className={styled.search}>
-            <AiOutlineSearch className={styled.icon} />
-            <input placeholder="Search" />
-            <AiOutlineClose className={styled.icon} />
+        <div className={styled.search_container}>
+            <div className={styled.search}>
+                <AiOutlineSearch className={styled.icon} />
+                <input placeholder="Search" />
+                <AiOutlineClose className={styled.icon} />
+            </div>
+            <button>Close</button>
         </div>
     )
 }
