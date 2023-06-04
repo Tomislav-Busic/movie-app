c4b9b4efcd759ac2aeaf2d4ea78aa739cfd19566
Tomislav Busic <tomislav.busic89@gmail.com>
2023-06-04 16:21:58 +0200

    testing

--- src/layouts/navbar/Navbar.test.tsx
+++ src/layouts/navbar/Navbar.test.tsx
@@ -2,20 +2,21 @@ import { render, fireEvent, screen } from "@testing-library/react";
 import { Navbar } from "./Navbar";
 
 
-
 //test block
-test("increments counter", () => {
+test("testing home location", () => {
     // render the component on virtual dom
     render(<Navbar />);
 
+
     //select the elements you want to interact with
-    const homebtn = screen.getByTestId("home_btn");
+    const homebtn = screen.getByTestId("homebtn");
 
     //interact with those elements
     fireEvent.click(homebtn);
-    let assignSpy = jest.spyOn(window.location, 'assign')
-    //assert the expected result
-    expect(assignSpy).toBe("http://localhost:3000/");
+
+    expect(window.location.search).toBe("http://localhost:3000/");
+    expect(window.location.href).not.toBeDefined();
+
 });
 
 
