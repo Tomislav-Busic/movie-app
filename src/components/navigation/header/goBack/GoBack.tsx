fdd91ab95ff44d4f315ae67041f7213904ee0c0b
Tomislav Busic <tomislav.busic89@gmail.com>
2023-06-09 19:32:14 +0200

    header left button

--- src/components/navigation/header/goBack/GoBack.tsx
+++ src/components/navigation/header/goBack/GoBack.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 import { AiOutlineLeft } from "react-icons/ai";
 
@@ -6,6 +7,12 @@ import styled from "./goBack.module.scss";
 
 export const GoBack = () => {
     const [active, setActive] = useState(false);
+    const location = useLocation();
+
+    useEffect(() => {
+        location.pathname !== "/" ? setActive(true) : setActive(false)
+    }, [location.pathname])
+
 
     return <>
         {active &&
