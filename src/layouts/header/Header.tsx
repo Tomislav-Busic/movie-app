99f6ea02d214e380c7329ad36998c24d0659b62e
Tomislav Busic <tomislav.busic89@gmail.com>
2023-06-09 16:41:43 +0200

    remove statusBar

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -1,11 +1,9 @@
 import { Logo } from "./logo/Logo";
-import { StatusBar } from "./statusbar/Statusbar";
 import styled from "./header.module.scss";
 
 export const Header = () => {
     return (
         <div className={styled.navbar}>
-            <StatusBar />
             <Logo />
         </div>
     )
