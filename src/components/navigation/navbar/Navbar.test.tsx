7ad8566a483fb23187ed621f6775bf9473e40327
Tomislav Busic <tomislav.busic89@gmail.com>
2023-06-26 20:47:59 +0200

    tests and corrections

--- src/components/navigation/navbar/Navbar.test.tsx
+++ src/components/navigation/navbar/Navbar.test.tsx
@@ -3,32 +3,32 @@ import "@testing-library/jest-dom";
 import { Navbar } from "./Navbar";
 import { MemoryRouter } from "react-router-dom";
 
-//test Home button
-test("testing home button", () => {
-  render(
-    <MemoryRouter>
-      <Navbar />
-    </MemoryRouter>
-  );
+describe("Navbar", () => {
+  it("should be active home button", () => {
+    render(
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>
+    );
 
-  const divElement = screen.getByRole("homeBlue");
-  const buttonElement = screen.getByText("Home");
-  fireEvent.click(buttonElement);
+    const divElement = screen.getByRole("homeBlue");
+    const buttonElement = screen.getByText("Home");
+    fireEvent.click(buttonElement);
 
-  expect(divElement).toBeInTheDocument();
-});
+    expect(divElement).toBeInTheDocument();
+  });
 
-//test Favorite button
-test("testing favorite button", async () => {
-  render(
-    <MemoryRouter>
-      <Navbar />
-    </MemoryRouter>
-  );
+  it("should be active favorite button", async () => {
+    render(
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>
+    );
 
-  const buttonElement = screen.getByText("Favorites");
-  fireEvent.click(buttonElement);
-  const divElement = await waitFor(() => screen.findByRole("favoriteBlue"));
+    const buttonElement = screen.getByText("Favorites");
+    fireEvent.click(buttonElement);
+    const divElement = await waitFor(() => screen.findByRole("favoriteBlue"));
 
-  expect(divElement).toBeInTheDocument();
+    expect(divElement).toBeInTheDocument();
+  });
 });
