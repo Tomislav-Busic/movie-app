6eedb6fd6160b297758f0810025105c3bd626b4a
Tomislav Busic <tomislav.busic89@gmail.com>
2023-07-21 19:44:00 +0200

    MovieHeader test

--- src/features/movie/components/movieHeader/MovieHeader.test.tsx
+++ src/features/movie/components/movieHeader/MovieHeader.test.tsx
@@ -1,25 +1,50 @@
 import { render, screen, waitFor } from "@testing-library/react";
-import { movie } from "fakeData/data";
 import { MovieHeader } from "./MovieHeader";
+
+const mockMovieHeaderProps = {
+  release_date: "01/01/2023",
+  backdrop_path: "/path.jpg",
+  runtime: 123,
+  genres: [{ id: 1, name: "name" }],
+  production_countries: [{ iso_3166_1: "US", name: "us" }],
+};
 
 describe("MovieHeader", () => {
   it("should render MovieHeader", () => {
-    const { getByText } = render(<MovieHeader movie={movie} />);
+    const { getByText } = render(
+      <MovieHeader
+        release_date={mockMovieHeaderProps.release_date}
+        production_countries={mockMovieHeaderProps.production_countries}
+        genres={mockMovieHeaderProps.genres}
+        runtime={mockMovieHeaderProps.runtime}
+        backdrop_path={mockMovieHeaderProps.backdrop_path}
+      />
+    );
 
     expect(getByText).not.toBeNull();
   });
 
   it("should render year, country, genres, time", async () => {
-    render(<MovieHeader movie={movie} />);
+    render(
+      <MovieHeader
+        release_date={mockMovieHeaderProps.release_date}
+        production_countries={mockMovieHeaderProps.production_countries}
+        genres={mockMovieHeaderProps.genres}
+        runtime={mockMovieHeaderProps.runtime}
+        backdrop_path={mockMovieHeaderProps.backdrop_path}
+      />
+    );
 
     const year = await waitFor(() => screen.findByTitle("year"));
     const country = await waitFor(() => screen.findByTitle("country"));
     const time = await waitFor(() => screen.findByTitle("time"));
     const genres = await waitFor(() => screen.findByTitle("genre"));
 
-    expect(year.textContent).toEqual(movie?.year);
-    expect(country.textContent).toEqual(movie?.country);
-    expect(time.textContent).toEqual(movie?.time);
-    expect(genres.textContent).toEqual(movie?.genre.join(", "));
+    expect(year.textContent).toEqual(mockMovieHeaderProps.release_date);
+    expect(country.textContent).toEqual(
+      mockMovieHeaderProps.production_countries
+    );
+    expect(time.textContent).toEqual(mockMovieHeaderProps.runtime);
+    expect(genres.textContent).toEqual(mockMovieHeaderProps.genres);
   });
 });
